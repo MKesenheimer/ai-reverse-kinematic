9af09054c8dd8ce75f185f5303d87f5ca9422d16
MKesenheimer <m.kesenheimer@gmx.net>
2025-06-26 17:05:26 +0200

    update

--- simulation.py
+++ simulation.py
@@ -1,23 +1,38 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-# Define Sequential model with 3 layers
+if tf.config.list_physical_devices('GPU'):
+  print("TensorFlow **IS** using the GPU")
+else:
+  print("TensorFlow **IS NOT** using the GPU")
+
+# define Sequential model with 3 layers
 model = keras.Sequential(
     [
-        layers.Dense(2, activation="relu", name="layer1"),
-        layers.Dense(3, activation="relu", name="layer2"),
-        layers.Dense(4, name="layer3"),
+        layers.Dense(32, activation="relu", name="layer1", input_shape=(2,)),
+        layers.Dense(16, activation="relu", name="layer2"),
+        layers.Dense(1, activation='sigmoid', name="layer3"),
     ]
 )
 
+# compile
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
 
+# summary
 model.summary()
+keras.utils.plot_model(model, "my_first_model_with_shape_info.png", show_shapes=True)
 
-#keras.utils.plot_model(model, "my_first_model_with_shape_info.png", show_shapes=True)
+# train model on test data
+X = np.random.rand(3, 2)
+y = np.random.randint(0, 2, size=(3,))
+print(X)
+print(y)
 
-# Call model on a test input
-x = tf.ones((3, 2))
-y = model(x)
+model.fit(X, y, epochs=100, batch_size=8)
 
+y = model(X)
 print(y)
