9c7f96968061bf2df1667cea2304fb5a207e830d
MKesenheimer <m.kesenheimer@gmx.net>
2025-07-25 11:25:20 +0200

    3-Arm Roboter

--- renderer.py
+++ renderer.py
@@ -9,7 +9,7 @@ os.environ["SDL_RENDER_DRIVER"] = "opengl"
 
 import sdl3
 from drawer import Drawer
-from state import RobotState
+from robot import RobotState
 
 renderer = ctypes.POINTER(sdl3.SDL_Renderer)()
 window = ctypes.POINTER(sdl3.SDL_Window)()
@@ -22,6 +22,8 @@ robotState = RobotState()
 @sdl3.SDL_AppInit_func
 def SDL_AppInit(appstate, argc, argv):
     global robotArm1
+    global robotArm2
+    global robotArm3
 
     if not sdl3.SDL_Init(sdl3.SDL_INIT_VIDEO):
         sdl3.SDL_Log("Couldn't initialize SDL: %s".encode() % sdl3.SDL_GetError())
@@ -48,18 +50,59 @@ def SDL_AppInit(appstate, argc, argv):
     groundFixtureDef.density = 0
     groundBody.CreateFixture(groundFixtureDef)
 
-    # Create a arm body
+    # Create a arm1 body
+    robotArm1_length = float(robotState.get_length_arm1())
+    robotArm1_x = float(robotState.get_x_arm1())
+    robotArm1_y = float(robotState.get_y_arm1())
+    robotArm1_angle = float(robotState.get_angle_arm1())
     robotArm1Def = b2BodyDef()
     robotArm1Def.allowSleep = False
-    robotArm1Def.position = b2Vec2(200 / ppm, 210 / ppm)
+    robotArm1Def.position = b2Vec2(robotArm1_x / ppm, robotArm1_y / ppm)
+    robotArm1Def.angle = robotArm1_angle
     #robotArm1Def.type = b2_dynamicBody
     robotArm1 = world.CreateBody(robotArm1Def)
     robotArm1Shape = b2PolygonShape()
-    robotArm1Shape.SetAsBox(10 / 2 / ppm, 60 / 2 / ppm)
+    robotArm1Shape.SetAsBox(10 / 2 / ppm, robotArm1_length / 2 / ppm)
     robotArm1FixtureDef = b2FixtureDef()
     robotArm1FixtureDef.shape = robotArm1Shape
     robotArm1FixtureDef.density = 0
     robotArm1.CreateFixture(robotArm1FixtureDef)
+
+    # Create a arm2 body
+    robotArm2_length = float(robotState.get_length_arm2())
+    robotArm2_x = float(robotState.get_x_arm2())
+    robotArm2_y = float(robotState.get_y_arm2())
+    robotArm2_angle = float(robotState.get_angle_arm2())
+    robotArm2Def = b2BodyDef()
+    robotArm2Def.allowSleep = False
+    robotArm2Def.position = b2Vec2(robotArm2_x / ppm, robotArm2_y / ppm)
+    robotArm2Def.angle = robotArm2_angle
+    #robotArm2Def.type = b2_dynamicBody
+    robotArm2 = world.CreateBody(robotArm2Def)
+    robotArm2Shape = b2PolygonShape()
+    robotArm2Shape.SetAsBox(10 / 2 / ppm, robotArm2_length / 2 / ppm)
+    robotArm2FixtureDef = b2FixtureDef()
+    robotArm2FixtureDef.shape = robotArm2Shape
+    robotArm2FixtureDef.density = 0
+    robotArm2.CreateFixture(robotArm2FixtureDef)
+
+    # Create a arm3 body
+    robotArm3_length = float(robotState.get_length_arm3())
+    robotArm3_x = float(robotState.get_x_arm3())
+    robotArm3_y = float(robotState.get_y_arm3())
+    robotArm3_angle = float(robotState.get_angle_arm3())
+    robotArm3Def = b2BodyDef()
+    robotArm3Def.allowSleep = False
+    robotArm3Def.position = b2Vec2(robotArm3_x / ppm, robotArm3_y / ppm)
+    robotArm3Def.angle = robotArm3_angle
+    #robotArm3Def.type = b2_dynamicBody
+    robotArm3 = world.CreateBody(robotArm3Def)
+    robotArm3Shape = b2PolygonShape()
+    robotArm3Shape.SetAsBox(10 / 2 / ppm, robotArm3_length / 2 / ppm)
+    robotArm3FixtureDef = b2FixtureDef()
+    robotArm3FixtureDef.shape = robotArm3Shape
+    robotArm3FixtureDef.density = 0
+    robotArm3.CreateFixture(robotArm3FixtureDef)
 
     return sdl3.SDL_APP_CONTINUE
 
@@ -77,7 +120,14 @@ def SDL_AppIterate(appstate):
     sdl3.SDL_SetRenderDrawColor(renderer, 33, 33, 33, sdl3.SDL_ALPHA_OPAQUE)
     sdl3.SDL_RenderClear(renderer) # Start with a blank canvas
     
-    robotArm1.angle = float(robotState.get_angle_arm1())
+    robotArm1.angle = robotState.get_absolute_angle_arm1()
+    robotArm1.position = b2Vec2(robotState.get_x_arm1() / ppm, robotState.get_y_arm1() / ppm)
+
+    robotArm2.angle = robotState.get_absolute_angle_arm2()
+    robotArm2.position = b2Vec2(robotState.get_x_arm2() / ppm, robotState.get_y_arm2() / ppm)
+
+    robotArm3.angle = robotState.get_absolute_angle_arm3()
+    robotArm3.position = b2Vec2(robotState.get_x_arm3() / ppm, robotState.get_y_arm3() / ppm)
 
     world.Step(0.016, 3, 2)
     world.DrawDebugData()
--- simulation.py
+++ simulation.py
@@ -2,9 +2,32 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from state import RobotState
+from robot import RobotState
+import time
+import math
 
 robotState = RobotState()
+robotState.set_length_arm1(100)
+robotState.set_base_position_arm1((200, 250))
+robotState.set_angle_arm1(0)
+robotState.set_length_arm2(75)
+robotState.set_angle_arm2(0)
+robotState.set_length_arm3(50)
+robotState.set_angle_arm3(0)
+
+
+# Test: Update den Winkel des Roboterarms:
+while True:
+    angle1 = robotState.get_angle_arm1() + 0.01
+    robotState.set_angle_arm1(angle1)
+    angle2 = robotState.get_angle_arm2() + 0.02
+    robotState.set_angle_arm2(angle2)
+    angle3 = robotState.get_angle_arm3() + 0.03
+    robotState.set_angle_arm3(angle3)
+    x_top, y_top = tuple(x - y for x, y in zip(robotState.get_top_arm3(), robotState.get_base_position_arm1()))
+    y_top = -y_top
+    print(f"alpha = ({angle1 % (2 * math.pi):.1f}, {angle2 % (2 * math.pi):.1f}, {angle3 % (2 * math.pi):.1f}), (x, y) = ({x_top:.1f}, {y_top:.1f})")
+    time.sleep(0.02)
 
 if tf.config.list_physical_devices('GPU'):
   print("TensorFlow **IS** using the GPU")
@@ -33,7 +56,7 @@ keras.utils.plot_model(model, "my_first_model_with_shape_info_test.png", show_sh
 alpha = np.array([[0, 0], [1, 0], [0, 1]])
 coord = np.array([[0, 0], [1, 0], [1, 1]])
 
-model.fit(coord, alpha, epochs=100, batch_size=8)
+model.fit(coord, alpha, epochs=10, batch_size=8)
 
 print("Trainingsdaten:\nalpha:")
 print(alpha)
@@ -43,6 +66,3 @@ print(coord)
 print("\nBerchnete Winkel:")
 y = model(coord)
 print(y)
-
-# Test: Update den Winkel des Roboterarms:
-robotState.set_angle_arm1(0)
